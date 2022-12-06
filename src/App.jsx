27123e50c014540ce3a714a942aaade7d1916052
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-06 11:14:53 +0100

    🚧 Work in progress : setting module federation plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { StyledMain } from './setup/styled-components/styled_component';
 import GlobalStyle from './setup/styled-components/GlobalStyle';
+
+
 function App() {
 
   return(
 
     <>
    <GlobalStyle />
-   <StyledMain>Main</StyledMain>
+   <StyledMain>
+     
+   </StyledMain>
 </>
 );
 }
+
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,5 @@ export default defineConfig({
     shared: ["react", "react-dom"],
   })],
 
+
 })
